3fa31ec77ee7f82797fb23cfabb3ac3016593a7d
Pedro Naves <pedronaves@gmail.com>
2020-05-11 14:54:47 -0300

    Button Default Component

---
+++ src/components/BeagleButton/index.tsx
@@ -0,0 +1,39 @@
+/*
+  * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+
+import React, { FC } from 'react'
+import { StyledButton } from './styled'
+import { filterBooleanArray } from '../../utils/array'
+
+interface BeagleButtonInterface {
+	text: string;
+	onPress?: () => void
+	theme?: string;
+	className?: string;
+}
+
+const BeagleButton: FC<BeagleButtonInterface> = props => {
+	const { text, className, theme, onPress } = props
+	const validClass = filterBooleanArray([className, theme])
+	const classNames = validClass.join()
+	return (
+		<StyledButton className={classNames} onClick={onPress}>
+			{text}
+		</StyledButton>
+	)
+}
+
+export default BeagleButton
