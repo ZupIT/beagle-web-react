7fccd0596837796318129a9f9a657dac85ad4960
Rafael de Melo Silva <rafamsilva@live.com>
2020-07-10 15:31:57 -0300

    adjust root path

--- jest.config.js
+++ jest.config.js
@@ -132,7 +132,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "<rootDir>/__tests__/"
+    "<rootDir>/src/__tests__/"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
