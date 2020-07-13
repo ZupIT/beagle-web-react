8804ba816292406f296216a393faf76b9adc070a
james <012jamess@gmail.com>
2020-07-13 10:13:11 -0300

    add-beagle-files

---
+++ src/beagle/beagle-service.ts
@@ -0,0 +1,6 @@
+import { createBeagleUIService } from '@zup-it/beagle-react'
+
+export default createBeagleUIService({
+  baseUrl: "src/beagle/",
+  components: {}
+})
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { BeagleProvider, BeagleRemoteView } from '@zup-it/beagle-react';
+import BeagleService from '../beagle/beagle-service';
 
-const Home = () => <span>This is a boilerplate project for React + Typescript</span>
+function Home() {
+  return (
+    <BeagleProvider value={BeagleService}>
+      <BeagleRemoteView path={'/payload.json'} />
+    </BeagleProvider>
+  );
+}
 
-export default Home
+export default Home;
