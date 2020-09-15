4ffd59da8c19804a074a3867b5013460c2154bc9
Hector <hector.custodio@zup.com.br>
2020-09-15 17:04:53 -0300

    Fixes lint

--- packages/beagle-react-native/src/components/BeagleButton/index.tsx
+++ packages/beagle-react-native/src/components/BeagleButton/index.tsx
@@ -74,7 +74,7 @@ const BeagleButton: FC<BeagleButtonInterface> = ({
       minHeight: 60,
       maxHeight: 60,
       margin: 5,
-      minWidth: 200
+      minWidth: 200,
     },
   })
   return (
