516b88822b5e10c5264c3affd49c9c170783964b
james <jamesson.penha@zup.com.br>
2021-01-15 15:21:34 -0300

    add script init

--- packages/beagle-react/cli/init/boilerplate/app.tsx
+++ packages/beagle-react/cli/init/boilerplate/app.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import { BeagleProvider, BeagleRemoteView } from '@zup-it/beagle-react';
-import BeagleService from './beagle-service.js';
+import React from 'react'
+import './App.css'
+import { BeagleProvider, BeagleRemoteView } from '@zup-it/beagle-react'
+import BeagleService from './beagle/beagle-service'
 
 function App() {
   return (
     <BeagleProvider value={BeagleService}>
       <BeagleRemoteView route={'/welcome'} />
     </BeagleProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/beagle-react/cli/init/index.js
+++ packages/beagle-react/cli/init/index.js
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const fsPromises = fs.promises;
-const dir = 'src/beagle';
+const fs = require('fs')
+const fsPromises = fs.promises
+const dir = 'src/beagle'
 
 if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
+  fs.mkdirSync(dir)
 }
 
 (async () => {
-  const beagleContent = await readFileContent(__dirname + '/boilerplate/beagle-service.ts')
-  const appContent = await readFileContent(__dirname + '/boilerplate/app.tsx')
 
   async function readFileContent(path) {
     try {
       data = await fsPromises.readFile(path, (err, data) => {
-        if (err) throw err;
+        if (err) throw err
         else {
           data = data.toString('utf8')
         }
@@ -25,25 +23,28 @@ if (!fs.existsSync(dir)) {
     }
   }
 
-  const readline = require('readline');
+  const beagleContent = await readFileContent(__dirname + '/boilerplate/beagle-service.ts')
+  const appContent = await readFileContent(__dirname + '/boilerplate/app.tsx')
+  const readline = require('readline')
+  
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   })
 
   fs.writeFile('src/beagle/beagle-service.ts', beagleContent, function (err) {
-    if (err) throw err;
+    if (err) throw err
   })
 
   rl.question('Want to replace app.tsx with the Beagle configuration (y or n) ?', (answer) => {
     if (`${answer}` === 'y') {
       fs.writeFile('src/app.tsx', appContent, function (err) {
-        if (err) throw err;
-        process.exit();
-      });
+        if (err) throw err
+        process.exit()
+      })
     }
     else {
-      process.exit();
+      process.exit()
     }
   })
 })()
