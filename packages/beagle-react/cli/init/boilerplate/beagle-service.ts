49bd5710802c39f3862369c4504f97bc81b6eea5
jamessonpenhazup <65912159+jamessonpenhazup@users.noreply.github.com>
2021-02-12 15:42:25 -0300

    Feature/script init (#175)
    * add script init
    
    * add script init
    
    * add script init
    
    * add promises
    
    Co-authored-by: hectorcustodiozup <50678762+hectorcustodiozup@users.noreply.github.com>
    Co-authored-by: Tiago Peres França <tiagoperesfr@gmail.com>

---
+++ packages/beagle-react/cli/init/boilerplate/beagle-service.ts
@@ -0,0 +1,6 @@
+import { createBeagleUIService } from '@zup-it/beagle-react'
+
+export default createBeagleUIService({
+  baseUrl: 'http://usebeagle.io.s3-website-sa-east-1.amazonaws.com/start/',
+  components: {}
+})
