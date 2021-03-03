dbaf8af46e023faf8aecbcc2164458f03f6faeb0
Arthur Bleil <78227085+arthurbleilzup@users.noreply.github.com>
2021-03-03 15:38:53 -0300

    Allow text component to render not only strings (#183)
    * accessibility on the components
    
    * accessibility on components + tests
    
    * tiago improvements
    
    * allow text to render not only strings
    
    * fix react-native
    
    Co-authored-by: Arthur Bleil <bleil.arthur@gmail.com>

--- packages/beagle-react/src/components/BeagleText/index.tsx
+++ packages/beagle-react/src/components/BeagleText/index.tsx
@@ -1,37 +1,51 @@
 /*
-  * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *  http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-*/
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import { logger } from '@zup-it/beagle-web'
 import { BeagleTextInterface } from 'common/models'
 import { buildAccessibility } from '../../../../common/utils/accessibility'
 import withTheme from '../utils/withTheme'
 import { StyledText } from './styled'
 
-const BeagleText: FC<BeagleTextInterface> = props => {
+const BeagleText: FC<BeagleTextInterface> = (props) => {
   const { text, className, textColor, alignment, style, accessibility } = props
+  const [renderedText, setRenderedText] = useState<string>('')
   const a11y = buildAccessibility(accessibility)
 
+  useEffect(() => {
+    try {
+      if (text && typeof text === 'object') {
+        setRenderedText(JSON.stringify(text))
+      } else setRenderedText((text && typeof text !== 'function') ? String(text) : '')
+    } catch (error) {
+      logger.error(error)
+      setRenderedText('')
+    }
+  }, [text])
+
   return (
-    <StyledText 
-      textColor={textColor} 
-      alignment={alignment} 
-      className={className} 
+    <StyledText
+      textColor={textColor}
+      alignment={alignment}
+      className={className}
       style={style}
-      {...a11y}>
-      {text}
+      {...a11y}
+    >
+      {renderedText}
     </StyledText>
   )
 }
