1c74cd864f07d76118499140f68aee8e1a9fcb43
tayronearaujo <tayron_machado@hotmail.com>
2021-03-24 15:23:05 -0300

    fix input test

--- packages/beagle-react/src/__tests__/components/BeagleInput.spec.tsx
+++ packages/beagle-react/src/__tests__/components/BeagleInput.spec.tsx
@@ -24,7 +24,6 @@ let wrapper: any
 const beagleInputPropsMock: BeagleTextInputInterface = {
   value: 'Testing',
   placeholder: 'Testing',
-  enabled: false,
   disabled: false,
   readOnly: false,
   type: 'TEXT',
@@ -70,7 +69,6 @@ test('Should update text on input', () => {
   const wrapper = mount(
     <BeagleInput
       value={inputStateValue}
-      enabled={false}
       disabled={false}
       readOnly={false}
       placeholder="Testing"
