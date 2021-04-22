34a8cdc006bef04887fa6b2dcbf687fa5f94c5d1
james <jamesson.penha@zup.com.br>
2021-04-22 15:10:20 -0300

    requested changes

--- packages/beagle-react/cli/init/index.js
+++ packages/beagle-react/cli/init/index.js
@@ -1,34 +1,44 @@
 (async () => {
-  const fs = require('fs')
-  const fsPromises = fs.promises
+  const { constants, promises } = require('fs')
+  const { access, mkdir, readFile, writeFile } = promises
   const fileOptions = { encoding: 'utf8' }
   const dir = "src"
-  const beagleContent = await fsPromises.readFile(__dirname + '/boilerplate/beagle-service.ts')
-  const appContent = await fsPromises.readFile(__dirname + '/boilerplate/App.tsx')
+  const beagleContent = await readFile(__dirname + '/boilerplate/beagle-service.ts')
+  const appContent = await readFile(__dirname + '/boilerplate/App.tsx')
   const readline = require('readline')
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   })
 
-  await fs.promises.mkdir(`${dir}/beagle/`, { recursive: true });
-  await fsPromises.writeFile(`${dir}/beagle/beagle-service.ts`, beagleContent, fileOptions)
+  await mkdir(`${dir}/beagle/`, { recursive: true });
+  await writeFile(`${dir}/beagle/beagle-service.ts`, beagleContent, fileOptions)
 
   const createAppTsx = async (path, content) => {
     try {
-      await fsPromises.writeFile(`${path}/app.tsx`, content)
+      await writeFile(`${path}/app.tsx`, content)
       process.exit()
     } catch (error) {
       console.error(error)
       process.exit(1)
     }
   }
-  if (!fs.existsSync(`${dir}/app.tsx`)) {
+  const appTsxExists = async (path) => {
+    try {
+      await access(`${path}/app.tsx`, constants.F_OK)
+      return true
+    } catch (error) {
+      console.error("app.tsx did not exist, it was created with Beagle settings.")
+      return false
+    }
+  }
+  const appFileExists = await appTsxExists(dir)
+  if (!appFileExists) {
     createAppTsx(dir, appContent)
   }
   else {
     rl.question('Do you want to replace "app.tsx" content with the Beagle configuration (y or n)?', (answer) => {
-      if (`${answer}` === 'y' || 'Y') {
+      if (`${answer}` === 'y' || `${answer}` === 'Y') {
         createAppTsx(dir, appContent)
       }
       else {
